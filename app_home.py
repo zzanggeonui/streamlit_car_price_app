4991bc99e096d82983f60ed8688d21cdc0e0a4b1
zzanggeonui <120348599+zzanggeonui@users.noreply.github.com>
2022-12-20 11:35:58 +0900

    홈수정

--- app_home.py
+++ app_home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def run_home_app():
-    st.text('환영합니다')
+    st.text('안녕하세요')
     st.text('이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용입니다. 데이터 분석 및 고객 정보를 넣으면, 얼마정도의 차를 구매할지 예측해 줍니다. ')
     url = 'https://p4.wallpaperbetter.com/wallpaper/122/597/209/2019-cars-4k-dodge-challenger-rt-widebody-wallpaper-preview.jpg'
     st.image(url)
